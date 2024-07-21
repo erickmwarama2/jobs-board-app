c5d38ce6731e2bec579570286dfd6c026ecd58f4
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-07-21 20:57:04 +0300

    add create job mutation

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,32 +1,56 @@
-import { getJobs, getJob, getJobsByCompany } from "./db/jobs.js";
+import { getJobs, getJob, getJobsByCompany, createJob } from "./db/jobs.js";
 import { getCompany } from "./db/companies.js";
+import { GraphQLError } from "graphql";
 
 export const resolvers = {
-    Query: {
-        job: (_root, args) => {
-            // console.log(args);
-            const { id } = args;
-            return getJob(id);
-        },
-        jobs: async () => {
-            return await getJobs();
-        },
-        company: (_root, args) => {
-            const { id } = args;
-            return getCompany(id);
-        }
+  Query: {
+    job: async (_root, args) => {
+      // console.log(args);
+      const { id } = args;
+      const job = await getJob(id);
+
+      if (!job) {
+        throw notFoundError("No job found with id " + id);
+      }
+      return job;
     },
-    Job: {
-        date: (job) => toIsoDate(job.createdAt),
-        company: async (job) => {
-            return await getCompany(job.companyId);
-        }
+    jobs: async () => {
+      return await getJobs();
     },
-    Company: {
-        jobs: (company) => getJobsByCompany(company.id)
+    company: async (_root, args) => {
+      const { id } = args;
+      const company = await getCompany(id);
+      if (!company) {
+        throw notFoundError("No company found with id " + id);
+      }
+      return company;
+    },
+  },
+  Job: {
+    date: (job) => toIsoDate(job.createdAt),
+    company: async (job) => {
+      return await getCompany(job.companyId);
+    },
+  },
+  Company: {
+    jobs: (company) => getJobsByCompany(company.id),
+  },
+  Mutation: {
+    createJob: async (_root, { input: { title, description }}) => {
+        const companyId = "FjcJCHJALA4i"; // to change later
+        return await createJob({ companyId, title, description });
     }
+  }
 };
 
+function notFoundError(message) {
+  return new GraphQLError(message, {
+    extensions: {
+      code: "NOT_FOUND",
+    },
+  });
+}
+
 function toIsoDate(value) {
-    return new Date(value).toDateString();
+  return new Date(value).toDateString();
 }
