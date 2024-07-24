eac0871f251a4fd7a5a832689712d39102ffbd99
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-07-24 23:04:40 +0300

    remove unused hooks

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import JobList from '../components/JobList';
 import { useJobs } from '../lib/graphql/hooks.js';
 
--- client/src/pages/JobPage.js
+++ client/src/pages/JobPage.js
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import { formatDate } from '../lib/formatters';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 // import { getJob } from '../lib/graphql/queries';
 import { useJob } from '../lib/graphql/hooks.js';
 
